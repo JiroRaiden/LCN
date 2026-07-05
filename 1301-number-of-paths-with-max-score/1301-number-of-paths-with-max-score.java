943b1e6b6417eaad8346c37d4375287388fd3c6d
Bishal Mandi <bishalmandi428@gmail.com>
2026-07-06 02:09:35 +0530

    Time: 19 ms (31.03%), Space: 44.7 MB (85.06%) - LeetHub

---
+++ 1301-number-of-paths-with-max-score/1301-number-of-paths-with-max-score.java
@@ -0,0 +1,75 @@
+class Solution {
+    static final int MOD = 1_000_000_007;
+
+    public int[] pathsWithMaxScore(List<String> board) {
+        int n = board.size();
+
+        int[][] dp = new int[n][n];      // maximum score
+        int[][] ways = new int[n][n];    // number of ways
+
+        for (int i = 0; i < n; i++) {
+            Arrays.fill(dp[i], -1);
+        }
+
+        dp[n - 1][n - 1] = 0;
+        ways[n - 1][n - 1] = 1;
+
+        for (int i = n - 1; i >= 0; i--) {
+            for (int j = n - 1; j >= 0; j--) {
+
+                char ch = board.get(i).charAt(j);
+
+                if (ch == 'X' || (i == n - 1 && j == n - 1))
+                    continue;
+
+                int best = -1;
+                long cnt = 0;
+
+                // from below
+                if (i + 1 < n && dp[i + 1][j] != -1) {
+                    if (dp[i + 1][j] > best) {
+                        best = dp[i + 1][j];
+                        cnt = ways[i + 1][j];
+                    } else if (dp[i + 1][j] == best) {
+                        cnt = (cnt + ways[i + 1][j]) % MOD;
+                    }
+                }
+
+                // from right
+                if (j + 1 < n && dp[i][j + 1] != -1) {
+                    if (dp[i][j + 1] > best) {
+                        best = dp[i][j + 1];
+                        cnt = ways[i][j + 1];
+                    } else if (dp[i][j + 1] == best) {
+                        cnt = (cnt + ways[i][j + 1]) % MOD;
+                    }
+                }
+
+                // from diagonal
+                if (i + 1 < n && j + 1 < n && dp[i + 1][j + 1] != -1) {
+                    if (dp[i + 1][j + 1] > best) {
+                        best = dp[i + 1][j + 1];
+                        cnt = ways[i + 1][j + 1];
+                    } else if (dp[i + 1][j + 1] == best) {
+                        cnt = (cnt + ways[i + 1][j + 1]) % MOD;
+                    }
+                }
+
+                if (best == -1)
+                    continue;
+
+                int val = 0;
+                if (ch >= '1' && ch <= '9')
+                    val = ch - '0';
+
+                dp[i][j] = best + val;
+                ways[i][j] = (int) (cnt % MOD);
+            }
+        }
+
+        if (ways[0][0] == 0)
+            return new int[]{0, 0};
+
+        return new int[]{dp[0][0], ways[0][0]};
+    }
+}
