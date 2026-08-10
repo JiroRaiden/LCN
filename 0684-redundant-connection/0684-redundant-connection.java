e5c9931e1e3ed3d2aaa3d5e0366c3b18527ae9fd
Bishal Mandi <bishalmandi428@gmail.com>
2026-08-10 23:44:39 +0530

    Time: 20 ms (5.5%), Space: 47.5 MB (7.11%) - LeetHub

--- 0684-redundant-connection/0684-redundant-connection.java
+++ 0684-redundant-connection/0684-redundant-connection.java
@@ -1,50 +1,65 @@
 class Solution {
+    class Node
+    {
+        int first;
+        int second;
 
-    int[] parent;
-
+        Node(int first, int second)
+        {
+            this.first = first;
+            this.second = second;
+        }
+    }
     public int[] findRedundantConnection(int[][] edges) {
-
+        List<List<Integer>> adj = new ArrayList<>();
         int n = edges.length;
 
-        parent = new int[n + 1];
+        for(int i=0;i<=n;i++)
+        adj.add(new ArrayList<>());
 
-        // Initially, every node is its own parent
-        for (int i = 1; i <= n; i++) {
-            parent[i] = i;
-        }
 
-        for (int[] edge : edges) {
-
+        for(int[] edge: edges)
+        {
+            boolean[]vis = new boolean[n+1];
             int u = edge[0];
             int v = edge[1];
-
-            // If both nodes already have the same root,
-            // adding this edge creates a cycle
-            if (find(u) == find(v)) {
-                return edge;
+            if(isCycle(adj,u,v,vis))
+            {
+                return new int[]{u,v};
             }
 
-            // if hte parents are not same connect their components
-            union(u, v);
+            adj.get(u).add(v);
+            adj.get(v).add(u);
         }
 
         return new int[0];
     }
+    public boolean isCycle(List<List<Integer>> adj, int u , int v, boolean[]vis)
+    {
+        Queue<Node> q = new LinkedList<>();
 
-    private int find(int x) {
+        q.offer(new Node(u, -1));
+        vis[u]= true;
 
-        if (parent[x] != x) {
-            parent[x] = find(parent[x]);
+        while(!q.isEmpty())
+        {
+            int child = q.peek().first;
+            int parent = q.peek().second;
+            q.poll();
+
+            if(child==v) return true;
+
+
+            for(int i: adj.get(child))
+            {
+                if(!vis[i])
+                {
+                    vis[i] = true;
+                    q.add(new Node(i,child));
+                }
+            }
+        
         }
-
-        return parent[x];
-    }
-
-    private void union(int a, int b) {
-
-        int rootA = find(a);
-        int rootB = find(b);
-
-        parent[rootA] = rootB;
+        return false;
     }
 }
