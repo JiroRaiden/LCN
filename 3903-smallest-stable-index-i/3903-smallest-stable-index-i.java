f9e60105331ccb6d45821adb14bb3130a59da335
Bishal Mandi <bishalmandi428@gmail.com>
2026-09-04 23:52:39 +0530

    Time: 1 ms (99.79%), Space: 46.2 MB (88.3%) - LeetHub

---
+++ 3903-smallest-stable-index-i/3903-smallest-stable-index-i.java
@@ -0,0 +1,21 @@
+class Solution {
+    public int firstStableIndex(int[] nums, int k) {
+           int n = nums.length;
+
+        for (int i = 0; i < n; i++) {
+
+            int maxLeft = Integer.MIN_VALUE;
+            for (int j = 0; j <= i; j++) {
+                maxLeft = Math.max(maxLeft, nums[j]);
+            }
+            int minRight = Integer.MAX_VALUE;
+            for (int j = i; j < n; j++) {
+                minRight = Math.min(minRight, nums[j]);
+            }
+            if (maxLeft - minRight <= k) {
+                return i;
+            }
+        }
+        return -1;
+    }
+}
